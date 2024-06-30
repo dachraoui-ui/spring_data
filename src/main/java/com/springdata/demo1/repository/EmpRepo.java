f35c515ea3765549bf59fa8063c74eb377600b77
ahmed dacharoui <dachraouia193@gmail.com>
2024-06-30 20:14:57 +0100

    fffff

---
+++ src/main/java/com/springdata/demo1/repository/EmpRepo.java
@@ -0,0 +1,8 @@
+package com.springdata.demo1.repository;
+
+import com.springdata.demo1.model.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmpRepo extends JpaRepository<Employee,Integer> {
+
+}
---
+++ src/main/java/com/springdata/demo1/service/EmpService.java
@@ -0,0 +1,9 @@
+package com.springdata.demo1.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmpService {
+
+
+}
